e14d2b9fa90adc46185efe188cb093d36c86f451
sandjonyves <sandjonyves@gmail.com>
2025-07-08 08:49:38 +0200

    type of children correct

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,13 @@
 
 
 import '../globals.css';
+import { ReactNode } from "react";
 import I18nProvider from '@/container/I18nProvider';
-
-export default async function LocaleLayout({ children, params }) {
+interface LocaleLayoutProps {
+    children: ReactNode;
+    params: { locale: string };
+  }
+export default async function LocaleLayout({ children, params } :LocaleLayoutProps) {
   const { locale } = await params;
   return (
     <html lang={locale}>
